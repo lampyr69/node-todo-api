34e957bff32cd24473d8fe2c80a7d5cd07cd1eef
alampert <a.lampert@posteo.de>
2018-02-27 13:47:36 +0100

    add get todos id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo.js')
@@ -35,8 +36,41 @@ app.get('/todos', (req, res) => {
       res.send({ todos, code: 'Some useful information' })
     })
     .catch((err) => {
-      res.status(400).send(e)
+      res.status(400).send(err)
     })
+})
+
+//
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  console.log('ID', id)
+
+  if (!ObjectID.isValid(id)) {
+    console.log('Invalid object id', id)
+    return res.status(404).send({})
+  }
+
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        console.log('Valid id, but no todo found')
+        res.status(404).send({})
+      }
+
+      res.status(200).send({ todo })
+    })
+    .catch((e) => {
+      res.status(400).send({})
+    })
+
+  // validate id
+  // 404 -< send empty body
+  // query database findById
+  // success
+  // if(todo) send it back
+  // WSAECANCELLED back 404 with empty
+  // error
+  // 400 - nothing or error
 })
 
 // start the web app
